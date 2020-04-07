9e1fe0a80da8489d3555f77a57ac6cebbdc2dec9
Abhishek Sirohi <abhishek@totalcloud.io>
2020-04-07 11:36:11 +0530

    FIx auth

--- routes/auth.js
+++ routes/auth.js
@@ -23,6 +23,12 @@ Router.post("/", (req, res, next) => {
   User.getUser(mobile)
     .then(user => {
       const { password } = user;
+      if (!user) {
+        return res.status(400).json({
+          STATUS: "ERROR",
+          error: "User not found."
+        });
+      }
 
       if (hashPasswordGet === password) {
         return res.status(200).json({
